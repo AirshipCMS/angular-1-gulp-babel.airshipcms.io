43c4149ed527f0deac845ec0ad8efbd41cf9bfd4
Te Vallee <astronomemusic@gmail.com>
2017-06-05 15:06:25 -1000

    updated gulpfile and added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+#Summary
+This demo project demonstrates an Airship project that uses Angular 1.x + Gulp + Babel to render all pages.
+
+#Subdomain
+https://angular-1-gulp-babel.airshipcms.io
+
+#Build Tools
+gulp and babel compiles from outside of compartments (src/) into /compartments/assets/scripts/
+
+#How to Run Project
+
+`yarn install`
+`yarn start`
+
+open your browser to localhost:9001/
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
- 
-gulp.task('default', () => {
-  return gulp.src('src/app.js')
-    .pipe(babel({
-        presets: ['es2015']
-    }))
-    .pipe(gulp.dest('compartments/assets/scripts'));
+var gulp = require('gulp'),
+  babel = require('gulp-babel'),
+  http = require('http'),
+  st = require('st')
+
+gulp.task('js', ["es6"],function() {
+  return gulp.src("src/app.js")
+    .pipe(gulp.dest("compartments/assets/scripts"));
+});
+
+gulp.task("es6",function() {
+  return gulp.src("src/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("compartments/assets/scripts"));
+});
+
+
+gulp.task("default",["js"],function(done) {
+  http.createServer(
+    st({ index: 'compartments/layouts/application.html', cache: false, path: __dirname })
+  ).listen(8080, done);
+  gulp.watch('src/*', ['js']);
 });
